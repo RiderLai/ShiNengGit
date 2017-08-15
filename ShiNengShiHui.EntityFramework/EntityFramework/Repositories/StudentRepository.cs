b3b6b10f8e8bc7229f9642e99e7c4afab56d2658
lyf <lyf670354671@gmail.com>
2017-08-15 20:41:59 +0800

    2017-8-15 20:40:58 DbModelBuilder进行测试前

--- ShiNengShiHui.EntityFramework/EntityFramework/Repositories/StudentRepository.cs
+++ ShiNengShiHui.EntityFramework/EntityFramework/Repositories/StudentRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Abp.EntityFramework;
+using System.Data.Entity;
 
 namespace ShiNengShiHui.EntityFramework.Repositories
 {
@@ -16,9 +17,75 @@ namespace ShiNengShiHui.EntityFramework.Repositories
         {
         }
 
-        public override int Count()
+        //public override int Count()
+        //{
+        //    return Context.Database.ExecuteSqlCommand("Select Count(*) From " + TableName);
+        //}
+
+        public override IQueryable<Student> GetAll()
         {
-            return Context.Database.ExecuteSqlCommand("Select Count(*) From " + TableName);
+            var list = new List<Student>();
+            using (GetDbContext())
+            {
+                var students = Context.Database.SqlQuery<Student>("Select * From " + TableName);
+                list.AddRange(students);
+            }
+            return list.AsQueryable<Student>();
+        }
+
+        public override Student Insert(Student entity)
+        {
+            return base.Insert(entity);
+        }
+
+        public override Task<Student> InsertAsync(Student entity)
+        {
+            return base.InsertAsync(entity);
+        }
+
+        public override int InsertAndGetId(Student entity)
+        {
+            return base.InsertAndGetId(entity);
+        }
+
+        public override Task<int> InsertAndGetIdAsync(Student entity)
+        {
+            return base.InsertAndGetIdAsync(entity);
+        }
+
+        public override Student InsertOrUpdate(Student entity)
+        {
+            return base.InsertOrUpdate(entity);
+        }
+
+        public override Task<Student> InsertOrUpdateAsync(Student entity)
+        {
+            return base.InsertOrUpdateAsync(entity);
+        }
+
+        public override Student Update(Student entity)
+        {
+            return base.Update(entity);
+        }
+
+        public override Task<Student> UpdateAsync(Student entity)
+        {
+            return base.UpdateAsync(entity);
+        }
+
+        public override void Delete(int id)
+        {
+            base.Delete(id);
+        }
+
+        public override void Delete(Student entity)
+        {
+            base.Delete(entity);
+        }
+
+        protected override void AttachIfNot(Student entity)
+        {
+            base.AttachIfNot(entity);
         }
     }
 }
